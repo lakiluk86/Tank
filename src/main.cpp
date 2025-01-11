007a0ae980f10c667c2b8de00d95adc1d3351b4a
Lukas Frena <lukas.frena86@gmail.com>
2025-01-11 23:21:36 +0100

    increase precision

--- src/main.cpp
+++ src/main.cpp
@@ -16,31 +16,36 @@ const uint8_t MAX_DRIVE_SPEED_L = 85;  //maximum drive speed left track (duty cy
 const uint8_t MAX_DRIVE_SPEED_R = 85;  //maximum drive speed right track (duty cycle, absolut maximum is 255)
 const uint8_t DRIVE_SPEED_BACK_L = 70; //fixed speed for driving backwards left track (duty cycle, absolut maximum is 255)
 const uint8_t DRIVE_SPEED_BACK_R = 70; //fixed speed for driving backwards right track (duty cycle, absolut maximum is 255)
-const uint8_t TURRENT_ROTATE_STOP = 19; //duty cycle for middle position of servo for rotating turrent
+const uint16_t TURRENT_ROTATE_STOP = 305; //duty cycle for middle position of servo for rotating turrent
 const uint8_t DRIVE_JOY_DEATHBAND = 40; //deathband of left joystick axis for driving
-const uint8_t TURRENT_JOY_DEATHBAND = 40; //deathband of right joystick axis for rotating and elevating turrent
-const uint8_t TURRENT_ELEVATE_MIN = 8; //minimum duty cycle for turrent elevation
-const uint8_t TURRENT_ELEVATE_MAX = 30; //maximum duty cycle for turrent elevation
-const uint8_t TURRENT_ELEVATE_DFLT = TURRENT_ELEVATE_MIN + (TURRENT_ELEVATE_MAX - TURRENT_ELEVATE_MIN) / 2;
+const uint8_t TURRENT_JOY_DEATHBAND = 50; //deathband of right joystick axis for rotating and elevating turrent
+const uint16_t TURRENT_ELEVATE_MIN = 106; //minimum duty cycle for turrent elevation
+const uint16_t TURRENT_ELEVATE_MAX = 435; //maximum duty cycle for turrent elevation
+const uint16_t TURRENT_ELEVATE_DFLT = TURRENT_ELEVATE_MIN + (TURRENT_ELEVATE_MAX - TURRENT_ELEVATE_MIN) / 2;
 
 //PWM settings for drive control and servos
-const int MOTOR_L_1_PWM_CHANNEL = 0;
-const int MOTOR_L_2_PWM_CHANNEL = 1;
-const int MOTOR_R_1_PWM_CHANNEL = 2;
-const int MOTOR_R_2_PWM_CHANNEL = 3;
-const int TURRENT_ROTATE_PWM_CHANNEL = 4;
-const int TURRENT_ELEVATE_PWM_CHANNEL = 5;
-const int PWM_FREQ_DRIVE = 30000; //pwm frequency for drive control
-const int PWM_FREQ_SERVO = 50; //pwm frequency for servos
-const int PWM_RESOLUTION = 8; //max duty cycle is 255 because of this resolution
+const uint8_t MOTOR_L_1_PWM_CHANNEL = 0;
+const uint8_t MOTOR_L_2_PWM_CHANNEL = 1;
+const uint8_t MOTOR_R_1_PWM_CHANNEL = 2;
+const uint8_t MOTOR_R_2_PWM_CHANNEL = 3;
+const uint8_t TURRENT_ROTATE_PWM_CHANNEL = 4;
+const uint8_t TURRENT_ELEVATE_PWM_CHANNEL = 5;
+const uint32_t PWM_FREQ_DRIVE = 30000; //pwm frequency for drive control
+const uint8_t PWM_FREQ_SERVO = 50; //pwm frequency for servos
+const uint8_t PWM_RESOLUTION_DRIVE = 8; //max duty cycle is 255 because of this resolution
+const uint8_t PWM_RESOLUTION_SERVO = 12; //max duty cycle is 4095 because of this resolution
 
 //Commands for driving and servos (pwm duty cycle)
 uint8_t driveCmdLeft = 0;
 uint8_t driveCmdRight = 0;
 int8_t driveDirLeft = 0;
 int8_t driveDirRight = 0;
-uint8_t turrentRotateCmd = 0;
-uint8_t turrentElevateCmd = TURRENT_ELEVATE_DFLT;
+uint16_t turrentRotateCmd = 0;
+uint16_t turrentElevateCmd = TURRENT_ELEVATE_DFLT;
+long rotateLeftTimestamp = 0;
+long rotateRightTimestamp = 0;
+bool turrentRotateLeft = false;
+bool turrentRotateRight = false;
 long elevationTimestamp = 0;
 bool turrentElevateUp = false;
 bool turrentElevateDown = false;
@@ -70,12 +75,12 @@ void setup() {
   unbindAllBT();  //unbind all earlier BT devices
 
   //init pwm outputs drive and servos
-  ledcSetup(MOTOR_L_1_PWM_CHANNEL, PWM_FREQ_DRIVE, PWM_RESOLUTION);
-  ledcSetup(MOTOR_L_2_PWM_CHANNEL, PWM_FREQ_DRIVE, PWM_RESOLUTION);
-  ledcSetup(MOTOR_R_1_PWM_CHANNEL, PWM_FREQ_DRIVE, PWM_RESOLUTION);
-  ledcSetup(MOTOR_R_2_PWM_CHANNEL, PWM_FREQ_DRIVE, PWM_RESOLUTION);
-  ledcSetup(TURRENT_ROTATE_PWM_CHANNEL, PWM_FREQ_SERVO, PWM_RESOLUTION);
-  ledcSetup(TURRENT_ELEVATE_PWM_CHANNEL, PWM_FREQ_SERVO, PWM_RESOLUTION);
+  ledcSetup(MOTOR_L_1_PWM_CHANNEL, PWM_FREQ_DRIVE, PWM_RESOLUTION_DRIVE);
+  ledcSetup(MOTOR_L_2_PWM_CHANNEL, PWM_FREQ_DRIVE, PWM_RESOLUTION_DRIVE);
+  ledcSetup(MOTOR_R_1_PWM_CHANNEL, PWM_FREQ_DRIVE, PWM_RESOLUTION_DRIVE);
+  ledcSetup(MOTOR_R_2_PWM_CHANNEL, PWM_FREQ_DRIVE, PWM_RESOLUTION_DRIVE);
+  ledcSetup(TURRENT_ROTATE_PWM_CHANNEL, PWM_FREQ_SERVO, PWM_RESOLUTION_SERVO);
+  ledcSetup(TURRENT_ELEVATE_PWM_CHANNEL, PWM_FREQ_SERVO, PWM_RESOLUTION_SERVO);
   ledcAttachPin(MOTOR_L_1_GPIO, MOTOR_L_1_PWM_CHANNEL);
   ledcAttachPin(MOTOR_L_2_GPIO, MOTOR_L_2_PWM_CHANNEL);
   ledcAttachPin(MOTOR_R_1_GPIO, MOTOR_R_1_PWM_CHANNEL);
@@ -155,14 +160,42 @@ void loop() {
       driveDirRight = -1;
     }
 
-    //check turrent control rotation
+    //check turrent control rotation by right joystick
     if (abs(PS4.RStickX()) >= TURRENT_JOY_DEATHBAND){
       //this servo has a very narrow working duty cycle because of modification to endless servo movement
-      turrentRotateCmd = map(PS4.RStickX(), -127, 127, TURRENT_ROTATE_STOP + 5, TURRENT_ROTATE_STOP - 5);
+      turrentRotateCmd = map(PS4.RStickX(), -127, 127, TURRENT_ROTATE_STOP + 10, TURRENT_ROTATE_STOP - 10);
+    }
+
+    //check turrent control rotation by left/right buttons for fine controlling
+    if (PS4.Left() || (millis() - rotateLeftTimestamp < 250)){
+      if(turrentRotateLeft == false){
+        turrentRotateCmd = TURRENT_ROTATE_STOP + 4;
+        rotateLeftTimestamp = millis();
+        turrentRotateLeft = true;
+      }
+      if(millis() - rotateLeftTimestamp < 250){
+        turrentRotateCmd = TURRENT_ROTATE_STOP + 4;
+      }
+    }
+    else {
+      turrentRotateLeft = false;
+    }
+    if (PS4.Right() || (millis() - rotateRightTimestamp < 250)){
+      if(turrentRotateRight == false){
+        turrentRotateCmd = TURRENT_ROTATE_STOP - 4;
+        rotateRightTimestamp = millis();
+        turrentRotateRight = true;
+      }
+      if(millis() - rotateRightTimestamp < 250){
+        turrentRotateCmd = TURRENT_ROTATE_STOP - 4;
+      }
+    }
+    else {
+      turrentRotateRight = false;
     }
 
     //check turrent control elevation by right joystick
-    if (abs(PS4.RStickY()) >= TURRENT_JOY_DEATHBAND && (millis() - elevationTimestamp > 50)){
+    if (abs(PS4.RStickY()) >= TURRENT_JOY_DEATHBAND && (millis() - elevationTimestamp > 15)){
       if (PS4.RStickY() > 0){
         turrentElevateCmd++;
       }
@@ -174,10 +207,10 @@ void loop() {
       elevationTimestamp = millis();
     }
 
-    //check turrent control elevation by up/down buttons
+    //check turrent control elevation by up/down buttons for fine controlling
     if (PS4.Up()){
       if(turrentElevateUp == false){
-        turrentElevateCmd++;
+        turrentElevateCmd += 4;
         turrentElevateUp = true;
       }
     }
@@ -186,7 +219,7 @@ void loop() {
     }
     if (PS4.Down()){
       if(turrentElevateDown == false){
-        turrentElevateCmd--;
+        turrentElevateCmd -= 4;
         turrentElevateDown = true;
       }
     }
